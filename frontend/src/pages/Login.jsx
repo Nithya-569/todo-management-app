3b64374e1d77e185966da2c3d999b05b9dfcff60
MSNithyasree <msnithyasree102@gmail.com>
2026-07-21 16:57:42 +0530

    error got fixed in the frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,37 +1,48 @@
-import {Link} from 'react-router-dom';
+import {Link,useNavigate} from "react-router-dom";
 function Login(){
-    return{
-        <div className="container ch-100 d-flex justify-content-center align-items-center">
-            <div className="card shadow p-4" style={{width:400px}}>
-            <h2 className="text-center mb-4 "> Todo Manager</h2>
-            <form>
-            <div classame="mb-3">
-            <label className="form-label">Email</label>
-            <input 
-            type="email"
-            className="form-control"
-            placeholder="enter your mail "
-            />
-        </div>
-        <div className="mb-3">
-        <label className="form=label">Password</label>
-        <input 
-        type="password"
-        className="form-control"
-        placeholder="enter password"
-        />
-        </div>
-        <button className="btn btn-primary w-100">Login
-        </button>
-        </form>
-        <p className="text-center mt-3">
-        Don't have account?
-        <Link to="/signup"> Sign Up </Link>
-        </p>
-        </div>
-        </div>
-        
-
+    const navigate=useNavigate();
+    function handleLogin(e){
+        e.preventDefault();
+        navigate("/dashboard");
     }
+    return(
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-4">
+                    <div className="card p-4 shadow">
+                        <h2 className="text-centerr mb-4">Login</h2>
+                        <form onSubmit={handleLogin}>
+                            <div className="mb-3">
+                                <label>Email</label>
+                                <input  type="email"
+                                className="form-control"
+                                placeholder="enter email"
+                                required/>
+
+                            </div>
+                            <div className="mb-3">
+                                <label>Password</label>
+                                <input
+                                type="password"
+                                className="form-control"
+                                placeholder="enter password"
+                                required/>
+
+                            </div>
+                            <button type="submit"
+                            className="btn btn-primary w-100"
+                            >
+                                login
+                            </button>
+                        </form>
+                        <p className="text-center mt-3">
+                            don;t have accoutn ?
+                            <Link to="/signup"> SignUp</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Login;
